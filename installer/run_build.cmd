7932c60195e5963d1950f84563f885e74766fadc
byj1986 <byj1986@163.com>
2026-04-10 10:11:00 +0800

    install requirements before execute pyinstaller

--- installer/run_build.cmd
+++ installer/run_build.cmd
@@ -1,3 +1,7 @@
 @echo off
+cd /d "%~dp0.."
+python -m pip install -r requirements.txt
+if errorlevel 1 exit /b 1
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0build_and_install_startup.ps1"
-echo "Done."
+if errorlevel 1 exit /b 1
+echo Done.
